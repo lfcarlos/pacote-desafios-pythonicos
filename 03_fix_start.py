0e468bde336ea0f713e78b329e2f05017a5d5b82
lfcarlos <carloslf1@hotmail.com>
2020-10-13 20:08:11 -0300

    Solução do exercicío 3 - fix-start

--- 03_fix_start.py
+++ 03_fix_start.py
@@ -15,7 +15,8 @@ onde todas as instancias de stra foram substituidas por strb.
 
 def fix_start(s):
     # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) > 1:
+        return s[0] + s[1:].replace(s[0], '*')
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
