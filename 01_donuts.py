77cf0018c3d6da6a2475ead93cbffff7bb61b13d
lfcarlos <carloslf1@hotmail.com>
2020-10-13 20:16:20 -0300

    Solução do exercicío 1 - donuts

--- 01_donuts.py
+++ 01_donuts.py
@@ -11,7 +11,11 @@ e donuts(23) retorna 'Number of donuts: many'
 
 def donuts(count):
     # +++ SUA SOLUÇÃO +++
-    return
+    if count < 10:
+        donut = count
+    else:
+        donut = 'many'
+    return 'Number of donuts: ' + str(donut)
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
